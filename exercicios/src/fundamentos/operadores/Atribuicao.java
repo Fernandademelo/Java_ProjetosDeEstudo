73dd3436ba1e08b759eff3fc571ea9ac37b808d9
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-01-18 17:24:59 -0300

    operadores de atribuicao

---
+++ exercicios/src/fundamentos/operadores/Atribuicao.java
@@ -0,0 +1,22 @@
+package fundamentos.operadores;
+
+public class Atribuicao {
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c += b; // c = c + b;
+		c -= a; // c = c - a;
+		c *= b; // c = c * b;
+		c /= a; // c = c / a;
+		
+		
+		System.out.println(c);	
+		c %= a; // c = c % 2; 0 ou 1 (impar ou par)
+		
+		System.out.println(c);
+	}
+
+}
