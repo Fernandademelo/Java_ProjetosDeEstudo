ca52d6afe203c52972e8142099f099617df0077f
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-01-24 20:35:53 -0300

    el

---
+++ exercicios/src/fundamentos/exerciciosfimdomodulo/AreaTriangulo.java
@@ -0,0 +1,9 @@
+package fundamentos.exerciciosfimdomodulo;
+
+public class AreaTriangulo {
+public static void main(String[] args) {
+	//Criar um programa que leia o valor da base e da altura de um triângulo e calcule a área.
+	
+	
+}
+}
