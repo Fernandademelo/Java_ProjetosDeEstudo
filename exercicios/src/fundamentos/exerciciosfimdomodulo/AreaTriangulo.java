0426b6815c5ccc357c704e6e0e4eeb13fb125a48
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-01-26 12:15:07 -0300

    Mais exercicios

--- exercicios/src/fundamentos/exerciciosfimdomodulo/AreaTriangulo.java
+++ exercicios/src/fundamentos/exerciciosfimdomodulo/AreaTriangulo.java
@@ -1,9 +1,24 @@
 package fundamentos.exerciciosfimdomodulo;
 
+import java.util.Scanner;
+
 public class AreaTriangulo {
 public static void main(String[] args) {
 	//Criar um programa que leia o valor da base e da altura de um triângulo e calcule a área.
 	
+	Scanner entrada = new Scanner(System.in);
+	System.out.println("Qual o valor da base? ");
+	int base = entrada.nextInt();
 	
+	System.out.println("Qual o valor da altura? ");
+	int altura = entrada.nextInt();
+	
+	int area = ( base * altura ) / 2 ;
+	
+	System.out.println(" Área do Triangulo = " + area);
+	
+	
+	
+	entrada.close();
 }
 }
