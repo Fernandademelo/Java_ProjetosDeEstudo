c6224fd1d37cd2fd8154df9421a2cca46864859a
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-01-27 16:17:37 -0300

    while dt

--- exercicios/src/controle/DesafioDiaSemana.java
+++ exercicios/src/controle/DesafioDiaSemana.java
@@ -13,22 +13,25 @@ public class DesafioDiaSemana {
 		System.out.println("Escreva o dia da Semana");
 		String diaSemana = entrada.next();
 
-		if (diaSemana.equals("segunda")) {
+		if (diaSemana.equalsIgnoreCase("segunda")) {  
 			System.out.println("2");
-		} else if (diaSemana.equals("terça")) {
+		} else if (diaSemana.equalsIgnoreCase("terça")) {
 			System.out.println("3");
-		} else if (diaSemana.equals("quarta")) {
+		} else if (diaSemana.equalsIgnoreCase("quarta")) {
 			System.out.println("4");
-		} else if (diaSemana.equals("quinta")) {
+		} else if (diaSemana.equalsIgnoreCase("quinta")) {
 			System.out.println("5");
-		} else if (diaSemana.equals("sexta")) {
+		} else if (diaSemana.equalsIgnoreCase("sexta")) {
 			System.out.println("6");
-		} else if (diaSemana.equals("sabado")) {
+		} else if (diaSemana.equalsIgnoreCase("sabado")) {
 			System.out.println("7");
-		} else if (diaSemana.equals("domingo")) {
+		} else if (diaSemana.equalsIgnoreCase("domingo")) {
 			System.out.println("1");
 		}
-
+		//equalsIgnoreCase("domingo")
+				//verifica se a String é igual, ignorando letras maiusculas/minusculas diferentes.
 		entrada.close();
+		
+		
 	}
 }
