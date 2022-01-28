002672c380a9d4ea5a1440b55470b0d124616180
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-01-28 15:19:46 -0300

    do while exerc

---
+++ exercicios/src/controle/DoWhile.java
@@ -0,0 +1,20 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		String texto = "por favor"; // ele vai perguntar igualmente, o comando do acontece pelo menos uma vez
+		
+		do {
+			System.out.println("Você precisa falar as palavras mágicas...");
+			System.out.println("Você quer sair? ");
+			texto = entrada.nextLine();
+		} while(!texto.equalsIgnoreCase("por favor"));
+		
+		System.out.println("Obrigado!");
+		entrada.close();
+	}
+}
