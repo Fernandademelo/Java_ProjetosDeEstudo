1f774ed7663eddf6d137878d8916459f9fe0e96a
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-02-04 18:49:31 -0300

    g

--- exercicios/src/controle/DesafioWhile1.java
+++ exercicios/src/controle/DesafioWhile1.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class DesafioWhile1 {
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		int quantidadeDeNotas = 0;
 		double nota = 0;
-		double total =0;
-		 
-		while(nota != -1) {
+		double total = 0;
+
+		while (nota != -1) {
 			System.out.println("Informe a Nota ou (-1) pra Sair: ");
 			nota = entrada.nextDouble();
-			if(nota <= 10 && nota >= 0) {
+			if (nota <= 10 && nota >= 0) {
 				total += nota;
-				quantidadeDeNotas++;		
+				quantidadeDeNotas++;
 			} else if (nota != -1) {
 				System.out.println("Nota Inválida! ;D");
 			}
-			
+
 		}
-		
+
 //		Calcular media
 		double media = total / quantidadeDeNotas;
 		System.out.println("Media = " + media);
