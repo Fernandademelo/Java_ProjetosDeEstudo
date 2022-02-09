f200bf0a43a7e82bb87d222b38d345672cc73a06
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-02-09 14:56:15 -0300

    atv 3

---
+++ exercicios/src/controle/For3.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class For3 {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 0; // colocar fora do laço para poder acesar a var
+		for(; i < 10; i++) { // tirar a declaracao de dentro, senao só vai ate 9
+			System.out.println(i);
+		}
+		
+		System.out.println("Fora do laço:");
+		System.out.println(i);
+	}
+
+}
