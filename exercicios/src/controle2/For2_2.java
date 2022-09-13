b654e71140ff50a3883858953030cac38af1f4d7
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-09-12 22:15:32 -0300

    ++

---
+++ exercicios/src/controle2/For2_2.java
@@ -0,0 +1,10 @@
+package controle2;
+
+public class For2_2 {
+	public static void main(String[] args) {
+
+		for(int contador = 100; contador >= 0; contador -=2) {
+			System.out.printf("Contador= %d\n", contador);
+		}
+	}
+}
---
+++ exercicios/src/controle2/For3_2.java
@@ -0,0 +1,17 @@
+package controle2;
+
+public class For3_2 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]", i,j);
+			}
+			;
+		}
+
+//		int i = 0; // onde a var esta disponivel?
+//		System.out.println("saiu do for");
+//		System.out.println(i);
+	}
+}
