a8f53d8a9781af6e004348f54fd0510d0c1d5fb2
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-09-29 21:55:09 -0300

    + update

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -5,4 +5,15 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+		
+	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+		
+	} // dois metodos com o mesmo nome, o que tem aprametros só passar os parametros
+	
 }
