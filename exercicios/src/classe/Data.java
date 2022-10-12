ab1bb2d263398d0a74865745ad9535547dd5f4e7
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2022-10-11 22:21:06 -0300

    Construtores

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,6 +6,19 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data(){
+		dia = 1;
+		mes= 1;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial){
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+		
+		
+	}
 	// tipo obterDataFormatada (...) vou passar uma string
 	//nao passo parametros pqja esta contido na classe
 	
@@ -24,4 +37,4 @@ public class Data {
 	//nao funciona em apps, pq só funciona nos terminal.
 	//se retorna string, da pra suar em web, desktop, celular, mais flexivel.
 }
-}
+
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -7,18 +7,14 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		var d1 = new Data();
-		d1.dia = 2;
-		d1.mes = 12;
-		d1.ano = 1991;
+		d1.ano = 1987;
 		
 	
-		var d2 = new Data();
-		d2.dia = 31;
-		d2.mes = 12;
-		d2.ano = 2022;
+		var d2 = new Data(31,12,2020);
+
 
 		//utilixando o metodo:
-		System.out.printf(d1.obterDataFormatada());
+//		System.out.printf(d1.obterDataFormatada());
 		
 		System.out.printf(d2.obterDataFormatada());
 		
